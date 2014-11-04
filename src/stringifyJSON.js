854cc778a00f16f980c40d78b42376e153236fad
zdlopez <zachary.d.lopez@gmail.com>
2014-11-04 12:12:31 -0800

    Start done, recursive elements next.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,39 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  var result = '';
+  if(typeof obj === 'number'){
+    result += obj;
+  }
+  if(typeof obj === 'boolean'){
+    result += obj;
+  }
+  if(typeof obj === 'string'){
+    result += '\"' + obj + '\"';
+  }
+  if(typeof obj === 'object'){
+    if(Array.isArray(obj) === true){
+      var arr = [];
+      for(var i = 0; i<obj.length; i++){
+        arr.push(stringifyJSON(obj[i]));
+      }
+      if(arr.length === 0){
+        result += '[]';
+      } else {
+        result += '[' + arr + ']';
+      }
+    } else if(obj === null){
+      result = 'null';
+    } else {
+      result += '{}';
+    }
+
+
+  }
+  if (result === ''){
+    result = 'null';
+  }
+  
+  return result;
+
 };
